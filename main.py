212d8f1aac7335a57fece9a9fc5906f41b88a240
salmondavid <61057915+trigerme@users.noreply.github.com>
2022-01-27 01:59:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,66 @@
 #import Component/libreary/model
-<<<<<<< Updated upstream
+from asyncore import read
 from cgitb import text
-=======
->>>>>>> Stashed changes
+from cmath import inf
+from pickle import TRUE
 from pyexpat import model
+import queue
+import string
+import webbrowser
 from unittest import result
 from vosk import Model, KaldiRecognizer
 import json
 import os
 import pyaudio
 import pyttsx3
-<<<<<<< Updated upstream
 from core import SystemInfo
+from threading import Event
+import time
 
-=======
-from src import SystemInfo
->>>>>>> Stashed changes
 #speech Recg
 model = Model('model')
 rec = KaldiRecognizer(model, 16000)
 #speech Synt
-<<<<<<< Updated upstream
+
+#listener
 def speak(text):
     engine.say(text)
     engine.runAndWait()
-=======
->>>>>>> Stashed changes
 engine = pyttsx3.init()
 
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16,channels=1,rate=16000, input=True, frames_per_buffer=8000 )
+stream.chunk=1024 #this will break in chunks
 stream.start_stream()
 
-while True:
-    data = stream.read(4000)
-    if len(data) == 0:
-        break
+while TRUE:
+    data = stream.read(stream.chunk,exception_on_overflow=False) #infinity aya ba 
+    ##if  len(data) == 0 :
+      ##break
+    
     if rec.AcceptWaveform(data):
        result = rec.Result()
        result = json.loads(result)
-<<<<<<< Updated upstream
-       text = result['text']
-
-
-       if text == 'what time is' or text == 'tell me the time':
-            speak(SystemInfo.get_time())
+       print(result)
+       text= result['text']
        
-    
        
 
-=======
        
 
-       print (result["text"])
+       #Commands
 
->>>>>>> Stashed changes
-    
-    
+
+       if text == 'what is the date' or text == 'tell me the' :
+            speak(SystemInfo.get_date())
+          
+       if text == 'what is the time' or text == 'tell me' :
+            speak(SystemInfo.get_time()) 
+            
+       if text == 'open chrome' or text == 'run chrome'  :
+            speak('Openng google chrome') 
+            os.startfile("C:\Program Files\Google\Chrome\Application\chrome.exe")
+            
+       if text == 'what is your name' or text == 'tell me your name':
+            speak('My name is __')
+            
